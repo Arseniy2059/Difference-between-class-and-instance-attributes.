6d6cec16b509b422ee2d8bf2e8a8603836b937c7
Arseniy2059 <semyashulkovy64@mail.ru>
2024-10-01 12:56:28 +0300

    Update рллол.py

--- рллол.py
+++ рллол.py
@@ -1,6 +1,10 @@
 class House:
     houses_history = []
 
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors  # Характеристика
@@ -42,10 +46,7 @@ class House:
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __del__(self):
-        print("<название> снесён, но он останется в истории")
-
-    def __new__(cls, *args, **kwargs):
-        return houses_history
+        print(f"{self.name} снесён, но он останется в истории")
 
     def __add__(self, value):
         if isinstance(value, House):
